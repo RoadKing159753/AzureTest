c3c56370897303b76a1ae011cd39acdf4b3ef3a7
RoadKing159753 <111482881+RoadKing159753@users.noreply.github.com>
2022-11-10 09:28:56 +0530

    Update S3.tf

--- S3.tf
+++ S3.tf
@@ -16,20 +16,9 @@ provider "aws" {
 
 
 resource "aws_s3_bucket" "unrestricted" {
+  #zpc-skip-policy: ZS-AWS-00034:testing
   bucket = "examplebuckettftest"
   acl    = "private"
-  versioning {
-    enabled = true
-  }
-  logging {
-    target_bucket = "pavantestbucket"
-    target_prefix = "log/"
-    }
-}
-
-resource "aws_s3_bucket" "unrestricted123" {
-  bucket = "examplebuckettftest"
-  acl    = "public-read-write"
   versioning {
     enabled = true
   }
